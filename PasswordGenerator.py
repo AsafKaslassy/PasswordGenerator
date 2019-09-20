4b8dfdbd9382077463262d8736b06a098dc3801a
AsafKaslassy <44755590+AsafKaslassy@users.noreply.github.com>
2019-09-20 23:07:51 +0300

    better exceptions and error handling
    better exceptions and error handling

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,45 +1,57 @@
-import random
 import re
 import sys
+import string
+import random
 '''
 generates a password with
 with no duplicate characters in the password
 '''
 
-Easy = "abcdefghijklmnopqrstuvwxyz0123456789"
-Medium = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-Hard = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
+Easy = string.ascii_letters
+Medium = string.ascii_letters + string.punctuation
+Hard = string.ascii_letters + string.punctuation + string.digits
+numbers = list(range(1, 93))
+fail_message = " not a valid option ! try again"
+CRED = '\033[91m'
+CEND = '\033[1m'
 
 # password_complexity = [Easy,Medium,Hard]
 
 
-while True:
-    allowed = ['Easy','Medium','Hard']
-    allowed_lower = (list(map(lambda x:x.lower(),allowed)))
 
-    user_char_complexity = input ("Password Complexity (Easy/Medium/Hard) :  ")
-    # if not re.match("^[a-z]*$", user_char_complexity):
-    # if not re.match("Easy" or "Medium" or "Hard", user_char_complexity):
-    try:
-        if user_char_complexity == 'Easy' or 'Easy'.lower():
-            user_char_complexity = Easy
-        elif user_char_complexity == 'Medium' or 'medium':
-            user_char_complexity = Medium
-        elif user_char_complexity == 'Hard' or 'hard':
-            user_char_complexity = Hard
+def password_generator():
+    """
+    :return:
+    """
+    var = 1
+    while var == 1:
+        allowed = ['Easy','Medium','Hard']
+        allowed_lower = (list(map(lambda x:x.lower(),allowed)))
+        user_char_complexity = input ("Password Complexity (Easy/Medium/Hard) :  ")
+        if user_char_complexity in allowed_lower or allowed:
+            if user_char_complexity == 'a':
+                user_char_complexity = Hard
+            else:
+                print('\t',CRED+fail_message+CEND,'\n')
+                continue
+        else:
+            print('\t',CRED+fail_message+CEND,'\n')
+            continue
 
+        while var == 1:
+            try:
+                password_length = int(input ("Password Length (1-90):  "))
+                if password_length in numbers:
+                    password =  "".join(random.sample(user_char_complexity,password_length ))
+                    print ("your generated password is : ", password)
+                    break
+            except ValueError:
+                print(" not a valid option ! try again1")
+                continue
 
-    except ValueError:
-        print(user_char_complexity ," is not a valid option ! try again")
+            # TODO:
+            # check random.random
 
-    password_length = input ("Password Length :  ")
-    try:
-        password_length_num = int(password_length)
-    except ValueError:
-        print(password_length ," is not a valid password length number! try again")
-
-    password =  "".join(random.sample(user_char_complexity,password_length_num ))
-    print ("your generated password is : ", password)
-
-    # TODO:
-    # check random.random
+if __name__ == '__main__':
+    password_generator()
+    print('PassWord_Generator V1.0 started...')
